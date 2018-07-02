31fb59e751dd3786268bd330cd8210e1d0fbe951
ruslanrs <ruslanrs@code.edu.az>
2018-07-02 12:09:34 +0400

    Task

---
+++ WorkWithExcelFile/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace WorkWithExcelFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_Open = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.Location = new System.Drawing.Point(258, 334);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(94, 40);
+            this.btn_Submit.TabIndex = 0;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 265);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(317, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(58, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(484, 218);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_Open
+            // 
+            this.btn_Open.Location = new System.Drawing.Point(444, 327);
+            this.btn_Open.Name = "btn_Open";
+            this.btn_Open.Size = new System.Drawing.Size(88, 55);
+            this.btn_Open.TabIndex = 3;
+            this.btn_Open.Text = "Open";
+            this.btn_Open.UseVisualStyleBackColor = true;
+            this.btn_Open.Click += new System.EventHandler(this.btn_Open_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 434);
+            this.Controls.Add(this.btn_Open);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Submit);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_Open;
+    }
+}
+
---
+++ WorkWithExcelFile/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using ExcelDataReader;
+using System.Reflection;
+using System.Data.OleDb;
+
+
+namespace WorkWithExcelFile
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+       
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            ///Show Excel File
+            
+            string connectionString = @"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = c:\MainExample.xlsx; Extended Properties = 'Excel 12.0 Xml;HDR=YES'";
+
+            using (OleDbConnection ole = new OleDbConnection(connectionString))
+            {
+                try
+                {
+                    ole.Open();
+
+                    string sheetName = "Sheet1";
+
+                    using (OleDbCommand oleCommand = new OleDbCommand("SELECT * FROM[" + sheetName + "$]", ole))
+                    {
+                        using (OleDbDataReader reader = oleCommand.ExecuteReader())
+                        {
+                            DataTable dataTable = new DataTable("simple");
+
+                            dataTable.Load(reader);
+
+                            dataGridView1.DataSource = dataTable;
+                        }
+                    }
+                }
+                catch (OleDbException oleException)
+                {
+                    MessageBox.Show(oleException.Message);
+                }
+            }
+            
+            
+            ///End Showing Excel File
+
+
+            ///Failers)
+
+            //using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Excel Workbook 2010|*.xlsx", ValidateNames = true })
+            //{
+            //    if (ofd.ShowDialog() == DialogResult.OK)
+            //    {
+            //        FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+
+            //        IExcelDataReader reader = ExcelReaderFactory.CreateBinaryReader(fs);
+
+            //        reader.GetData();
+
+
+
+
+            //    }
+            //}
+
+
+            ///End Failers)
+
+        }
+
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            ///sql connection and Viewed Tables on Sql
+            
+            string connectionString = @"Data Source = .; Initial Catalog = LoginDb; Integrated Security = True";
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                SqlDataAdapter sqlData = new SqlDataAdapter("Select * FROM Users", sqlCon);
+
+                DataTable dtbl = new DataTable();
+
+                sqlData.Fill(dtbl);
+
+                dataGridView1.DataSource = dtbl;
+
+
+            }
+
+            /// end of Sql Showing ///
+
+        }
+    }
+}
